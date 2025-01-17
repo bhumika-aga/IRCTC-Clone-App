25b370fe22c2b4331657bc8ab4601f350b147f96
Bhumika Agarwal <bhumika.aga@gmail.com>
2025-01-17 22:49:38 +0530

    Project Initialization

---
+++ src/main/java/com/irctc/app/irctc_clone_app/IrctcCloneAppApplication.java
@@ -0,0 +1,13 @@
+package com.irctc.app.irctc_clone_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrctcCloneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrctcCloneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irctc/app/irctc_clone_app/IrctcCloneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irctc.app.irctc_clone_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrctcCloneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
